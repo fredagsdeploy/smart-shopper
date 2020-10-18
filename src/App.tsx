e2a26483a912f8b3d91aac3402a1b5a1f2478263
André Samuelsson <andre.samuelsson@navico.com>
2020-10-18 14:42:51 +0200

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,35 @@ import "./App.css";
 import { Provider } from "react-redux";
 import { store } from "./reducers/store";
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+
 import { ListPage } from "./ListPage";
 
 function App() {
   return (
     <Provider store={store}>
-      <ListPage />
+      <Router>
+        <Switch>
+          <Route path="/login">LoginPage</Route>
+          <Route path="/shoppingList">
+            <ListPage />
+          </Route>
+
+          <Route path="/">
+            <Redirect
+              to={{
+                pathname: "/login",
+              }}
+            />
+          </Route>
+        </Switch>
+      </Router>
     </Provider>
   );
 }
