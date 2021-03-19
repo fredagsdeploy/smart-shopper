26404584c557da72df155c1cf876ff7cd75c42d8
Johan Lindskogen <johan.lindskogen@gmail.com>
2021-03-19 18:48:34 +0100

    Add oauth2-proxy
    Make dockerfile for backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,57 +1,13 @@
 import express from "express";
-import { generateItemGraph } from "./item_graph_creator";
-import schedule from "node-schedule";
-
-const Influx = require("influx");
-const influx = new Influx.InfluxDB({
-  host: "localhost",
-  database: "telegraf",
-  schema: [],
-});
-
-let graph = {};
-
-influx
-  .query(
-    `
-    select * from "telegraf"."autogen"."item_checked" where time > now() - 28d ;
-  `
-  )
-  .then((entries) => {
-    console.log("first entires", entries);
-    graph = generateItemGraph(entries);
-  })
-  .catch((err) => {
-    console.log("Error, could not fetch entries from database.", err);
-    process.exit(1);
-  });
-
-// Update the state every midnight
-schedule.scheduleJob("0 0 * * * *", () => {
-  console.log("Running scheduled data fetch");
-  influx
-    .query(
-      `
-    select * from "telegraf"."autogen"."item_checked" where time > now() - 1d ;
-  `
-    )
-    .then((entries) => {
-      console.log("Entries fetched", entries);
-      graph = generateItemGraph(entries, graph);
-      console.log("Graph updated");
-    })
-    .catch((err) => {
-      console.log(
-        "Error, could not fetch entries from database during daily update",
-        err
-      );
-    });
-});
-
 const app = express();
-const port = 3001;
-app.get("/", (req, res) => {
-  res.send(graph);
+const port = 8080;
+app.get("/api", (req, res) => {
+
+  const userId = req.header("X-Forwarded-User")
+  const email = req.header("X-Forwarded-Email")
+
+
+  res.send({ hello: "world", userId, email });
 });
 app.listen(port, () => {
   return console.log(`server is listening osn ${port}`);
