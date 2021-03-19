f32d92196b87cda30ba7548a6fc1ad80c9b7135d
Joel Gustafsson <me@joelgustafsson.com>
2021-03-19 21:40:56 +0100

    require dotenv AEAP

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,17 @@
-import express, {Request} from "express";
-import {generateItemGraph} from "./neo4j_container";
-
 require('dotenv').config()
+
+import express, {Request} from "express";
+import {generateItemGraph} from "./neo4j_repository";
 
 
 const app = express();
 const port = 8080;
-app.get("/api", (req: Request<unknown, unknown, unknown, { storeName: string }>, res) => {
+app.get("/api", async (req: Request<unknown, unknown, unknown, { storeName: string }>, res) => {
 
   const userId = req.header("X-Forwarded-User")
   const email = req.header("X-Forwarded-Email")
 
   const storeName = req.query.storeName;
-
 
   if (storeName && userId) {
     const graph = await generateItemGraph(storeName, userId);
